275be1ea3d8f1b2dab8b3fad6916583cd0dc484a
Guilherme Varela <guilhermevarela@hotmail.com>
2017-09-30 20:02:49 -0300

    propbankbr: dataset parser

---
+++ propbankbr/propbankbr_parse_datasets.py
@@ -0,0 +1,8 @@
+'''
+	Author: Guilherme Varela
+
+	Performs dataset build according to refs/1421593_2016_completo
+	1. Merges PropBankBr_v1.1_Const.conll.txt and PropBankBr_v1.1_Dep.conll.txt as specified on 1421593_2016_completo
+	2. Parses new merged dataset into train and test, so it can be benchmarked by conll scripts 
+
+'''
