8bc2eeb60a19ad59df44395b16fa5a6d126eda78
Guilherme Varela <guilhermevarela@hotmail.com>
2017-10-27 19:38:19 -0200

    propbankbr: parsing 2 files

--- propbankbr/propbankbr_parser.py
+++ propbankbr/propbankbr_parser.py
@@ -28,22 +28,56 @@ MAPPER= {
 			'ARG4':14,
 			'ARG5':15,
 			'ARG6':16,
-			'ARG7':17,				
+		}
+	}, 
+	'DEP': { 
+		'filename': 'PropBankBr_v1.1_Dep.conll.txt',
+		'mappings': {
+			'ID':0,
+			'FORM':1,
+			'LEMMA':2,
+			'GPOS':3,
+			'MORF':4,
+			'DTREE':5,
+			'FUNC':6,
+			'PRED':8,
+			'ARG0':9,
+			'ARG1':10,
+			'ARG2':11,
+			'ARG3':12,
+			'ARG4':13,
+			'ARG5':14,
+			'ARG6':15,
 		}
 	}
 }
 
-def propbankbr_parser():
+
+def propbankbr_parser(tokens=[], verbs=[], verbose=True):		
+	filename= 	MAPPER['CONST']['filename']
+	mappings=   MAPPER['CONST']['mappings']
+
+	sentences1, predicates1, tags1=propbankbr_fileparser(filename, mappings, tokens=[], verbs=[])
+	if verbose:
+		print_sentence(sentences1,  0)
+
+	filename= 	MAPPER['DEP']['filename']
+	mappings=   MAPPER['DEP']['mappings']
+	sentences2, predicates2, tags2=propbankbr_fileparser(filename, mappings, sep=' ', tokens=[], verbs=[])	
+	if verbose: 
+		print_sentence(sentences2,  0)
+
+	return sentences1, predicates1, tags1
+
+def propbankbr_fileparser(filename, mappings, sep='\t',tokens=[], verbs=[]):
 	'''
-		propbankbr_parse_1 : generates development, validation and test sets according to 1421593_2016_completo
+		generates development, validation and test sets according to 1421593_2016_completo
 	'''
 
 	# How to represent the dataset?
 	# which datastructure best represents the dataset?
 	# An array of dictionaries
 	# 	each example is a dictionary
-	filename= 	MAPPER['CONST']['filename']
-	mappings=   MAPPER['CONST']['mappings']
 	imappings= {v:k for k,v in mappings.items()}
 
 	sentences= [] 	
@@ -54,8 +88,9 @@ def propbankbr_parser():
 	sentence=get_signature(mappings)
 	predicates={}
 	tags={}
-
+	print('#rows\t#sentences\t#predicates\t#arguments\t#tokens\t#verbs')
 	for line in open(filename):		
+		# import code; code.interact(local=dict(globals(), **locals()))
 		# every new sentence except the first is followed by
 		if ((f_count == 0) | (line == '\n')):			
 			s_count+=1
@@ -65,32 +100,48 @@ def propbankbr_parser():
 
 		if len(line)>1:
 			# import code; code.interact(local=dict(globals(), **locals()))
-			values= line.split('\t')
-			values= list(map(lambda s: s.replace(' ',''), values))			
-			for keys_count, val in enumerate(values): 
-				if keys_count in imappings.keys() or (keys_count>10):
-					if (keys_count<=10):
-						key=imappings[keys_count]	
+			# values= line.split('\t')
+			# values= list(map(lambda s: s.replace(' ',''), values))			
+			# import code; code.interact(local=dict(globals(), **locals()))
+			
+			data= line.split(sep)
+			data= filter(lambda s: s != '', data)			
+			data= map(lambda s: s.replace(' ',''), data)			
+			values= list(data)
 
-					if (keys_count>=10):						
-						val= val.translate(str.maketrans('','','\n()*')) 
-					sentence[key].append(val)
+			# values= list(filter(lambda s: s != '', values))			
+
+			for keys_count, val in enumerate(values): 
+				if keys_count in imappings.keys():
+					key=imappings[keys_count]	
 					
+					if (key[:3]=='ARG'):
+						val= val.translate(str.maketrans('','','\n()*')) 
+						sentence[key].append(val)
+					else:
+						sentence[key].append(val)
+
+					if (key=='LEMMA'):					
+						tokens.append(val)
+
 					if (key=='PRED'):
 						if val != '-':
+							verbs.append(val)
 							predicates[p_count]=s_count 		
 							p_count+=1
 
-					if (key[:3] == 'ARG'): # SPECIAL CASE WITH MULTIPLE ARGS										
+					if (key[:3] == 'ARG'): # SPECIAL CASE WITH MULTIPLE ARGS																
 						#updates arguments	
-						if val != '':
+						if ((val != '') and (val != 'V') and (val != '-')) :
 							tags[t_count]={ 
 								'pos':(f_count,s_count,p_count),				
-								'tags': val
+								'tag': val
 							}
 							t_count+=1
+							
 		f_count+=1		
-		sys.stdout.write('#rows:%05d\t#sentences:%05d\t#predicates:%03d#arguments:%05d\t\r' % (f_count, s_count, p_count, len(tags)))
+
+		sys.stdout.write('%05d\t%05d\t%03d\t%05d\t%05d\t%05d\r' % (f_count, s_count, p_count, len(tags), len(tokens), len(verbs)))
 		sys.stdout.flush()
 	print('')
 	sentences.append(sentence) # appends last sentence
@@ -98,6 +149,10 @@ def propbankbr_parser():
 	print('total sentences',len(sentences))
 	print('total propositions',len(predicates))
 	print('total arguments',len(tags))
+	print('tokens:%05d\tunique:%05d\t'% (len(tokens),len(set(tokens))))
+	print('verbs:%03d\tunique:%03d\t'% (len(verbs),len(set(verbs))))
+
+
 	# print(sentences[0]['ID']) 
 	return sentences, predicates, tags 
 
@@ -142,13 +197,45 @@ def print_header(values, nargs):
 def get_signature(mappings): 
 	return {k:[] for k in mappings}
 
+def get_arguments(tags):
+	arguments={}
+	for val in tags.values():
+		if val['tag'] in arguments:
+			arguments[val['tag']]+= 1
+		else:
+			arguments[val['tag']]=1
+	
+	print('total tags:', len(tags))
+	for key, value in arguments.items():
+		print(key, value)
+  
+	return arguments
+
+def get_arguments_length(sentences):
+	arguments_length={}
+	for sentence in sentences:
+		for key, array in sentence.items():
+			if key[:3] == 'ARG' and len(array)>0:
+				if key in arguments_length:
+					arguments_length[key]+= 1
+				else:
+					arguments_length[key]=1
+	
+	for key, value in arguments_length.items():
+		print(key, value)
+  
+	return arguments_length
+
+
 
 if __name__== '__main__':		
-	sentences, predicates, tags= propbankbr_parser()
+	tokens=[] 
+	sentences, predicates, tags= propbankbr_parser(tokens)
+	print(len(tokens))
 	print_sentence(sentences,  0)
-	print(sentences[0]['PRED'])
-	print(sentences[0]['ARG0'])
-	print(sentences[0]['ARG1'])
+
+	stats= get_arguments(tags)
+	stats_length= get_arguments_length(sentences)
 	
 
 
