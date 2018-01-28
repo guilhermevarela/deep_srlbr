8f5cb198d76c01b69003fcc79831023ff5554e8e
Guilherme Varela <guilhermevarela@hotmail.com>
2018-01-28 17:05:15 -0200

    data transformations: tfrecords

--- datasets/data_tfrecords.py
+++ datasets/data_tfrecords.py
@@ -1,31 +1,23 @@
 '''
-Created on Jan 26, 2018
+Created on Jan 25, 2018
 	@author: Varela
-	
-	Converts gesim w2v to Tensorflow's tensor
 
+	Generates tfrecords
+		*devel
+		*valid
+		*test
+	
 '''
-import pandas as pd
+import pandas as pd 
 import numpy as np 
+
+from data_propbankbr import  propbankbr_lazyload
+from data_embed import embed_input_lazyload, embed_output_lazyload
+
 import tensorflow as tf 
 
-
-# def proposition2sequence_examples(df, input_vocab=[], output_vocab=[], col_tokens=['LEMMA', 'M_R'], col_labels=['ARG']):
-# 	if not(vocab):
-# 		input_vocab= extract_vocab(df, column='LEMMA')
-
-# 	if not(vocab):
-# 		output_vocab= extract_vocab(df, column='ARG')		
-
-# 	np = max(df['P_S'])	# number of propositions
-# 	for p in range(1, np+1):
-# 		ex= proposition2sequence_example(
-# 			dataframe2dict( df[df['P_S']==p] ), 
-# 			input_vocab, 
-# 			output_vocab, 
-# 			col_tokens=col_tokens,
-# 			col_labels=col_labels
-# 		)
+EMBEDDING_PATH='embeddings/'
+TARGET_PATH='training/pre/00/'
 
 
 def proposition2sequence_example(prop_dict, input_vocab, output_vocab, features=['PRED'], sequence_keys=['LEMMA', 'M_R'], target_key=['LABEL']):
@@ -33,7 +25,6 @@ def proposition2sequence_example(prop_dict, input_vocab, output_vocab, features=
 	# A non-sequential feature of our example
 	sequence_length=len(prop_dict[target_key[0]])
 	ex.context.feature['length'].int64_list.value.append(sequence_length)
-	# import code; code.interact(local=dict(globals(), **locals()))			
 
 	for feature_name in features:
 		token= prop_dict[feature_name][0]
@@ -56,7 +47,6 @@ def proposition2sequence_example(prop_dict, input_vocab, output_vocab, features=
 	
 	return ex	
 	
-
 def df2data_dict(df):	
 	return df.to_dict(orient='list')
 
@@ -68,12 +58,28 @@ def data_dict2word2idx(data_dict, key='LEMMA'):
 
 
 
+if __name__== '__main__':
+	tfrecords_path= TARGET_PATH + 'devel2.tfrecords'
 
+	df=propbankbr_lazyload('zhou_devel')
+	np = max(df['P_S'])	# number of propositions
 
+	word2idx, _ =embed_input_lazyload()
+	klass2idx, _ =embed_output_lazyload()
+	with open(tfrecords_path, 'w+') as f:
+		writer= tf.python_io.TFRecordWriter(f.name)
 
-
-
-
+		for p in range(1, np+1):
+			df_prop= df[df['P_S']==p]
+			ex= proposition2sequence_example(
+				df2data_dict( df_prop ), 
+				word2idx, 
+				klass2idx 
+			)	
+			writer.write(ex.SerializeToString())        
+    
+		writer.close()
+		print('Wrote to {}'.format(f.name))		
 
 
 
