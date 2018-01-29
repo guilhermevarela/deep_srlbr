6a0c2703c342fb6178f3885c8d365e922ffaabe6
Guilherme Varela <guilhermevarela@hotmail.com>
2018-01-29 20:00:15 -0200

    ParsingInputs: Input refactoring (2)

--- tfrecord_read.py
+++ tfrecord_read.py
@@ -8,7 +8,7 @@ Created on Jan 28, 2018
 import numpy as np 
 import tensorflow as tf 
 
-from datasets.data_embed import embed_input_lazyload 
+from datasets.data_embed import embed_input_lazyload, embed_output_lazyload  
 
 TARGET_PATH='datasets/training/pre/00/'
 tfrecords_filename= TARGET_PATH + 'devel2.tfrecords'
@@ -38,10 +38,12 @@ def read_and_decode(filename_queue):
 	# INT<1>
 	predicate= 	 tf.sparse_tensor_to_dense(sequence_features['PRED'])	
 	#lemma<TIME,>
+
 	lemma= 	  tf.sparse_tensor_to_dense(sequence_features['LEMMA'])	
-	#mr<TIME,> of zeros and ones
+	#mr<TIME,> of zeros and ones	
 	mr= 	 		tf.sparse_tensor_to_dense(sequence_features['M_R'])	
 	#target<TIME,> of integers
+	# targets_sparse= sequence_features['targets']
 	targets= 	tf.sparse_tensor_to_dense(sequence_features['targets'])	
 
 
@@ -49,22 +51,23 @@ def read_and_decode(filename_queue):
 
 if __name__== '__main__':
 	word2idx, _embeddings= embed_input_lazyload()		
+	klass2idx, _klass_ind= embed_output_lazyload()		
 
 	embeddings= tf.constant(_embeddings.tolist(), shape=(len(_embeddings),50), dtype=tf.float32)
+	klass_ind= tf.constant(_klass_ind.tolist(), shape=_klass_ind.shape, dtype=tf.float32)
 
 	filename_queue= tf.train.string_input_producer([tfrecords_filename], num_epochs=1)
 	
-	length, idx_pred, idx_lemma, M_R, target= read_and_decode(filename_queue)	
+	length,  idx_pred, idx_lemma,  M_R, targets= read_and_decode(filename_queue)	
 		
 
 	#LEMMA<TIME,1,EMBEDDING_SIZE> --> <TIME,EMBEDDING_SIZE>
-	LEMMA = tf.nn.embedding_lookup(embeddings, idx_lemma)
+	LEMMA  = tf.nn.embedding_lookup(embeddings, idx_lemma)
 	#PRED<EMBEDDING_SIZE,> --> <1,EMBEDDING_SIZE> 
-	PRED  = tf.nn.embedding_lookup(embeddings, idx_pred)
+	PRED   = tf.nn.embedding_lookup(embeddings, idx_pred)
 
-	# NORM_PRED=tf.reshape(PRED,[1,50])
+	TARGET = tf.nn.embedding_lookup(klass_ind, targets)
 
-	# NORM_M_R= tf.expand_dims(M_R,2)
 	init_op = tf.group( 
 		tf.global_variables_initializer(),
 		tf.local_variables_initializer()
@@ -75,16 +78,18 @@ if __name__== '__main__':
 		threads= tf.train.start_queue_runners(coord=coord)
 
 		for i in range(1):
-			l, PRED, LEMMA, M_R, t = session.run([length, PRED, LEMMA, M_R, target])
+		
+			T, PRED, LEMMA,  M_R, TARGET =session.run([length, PRED, LEMMA,  M_R, TARGET])
 			
 
-			print('length',l)
+			print('length',T)
 			print('predicate',PRED.shape)
 			# print('norm predicate',NORM_PRED.shape)
 			print('lemma',LEMMA.shape)
 			# print('norm m_r',NORM_M_R.shape)
+			
 			print('mr', M_R.shape)
-			print('target',t)
+			print('target',TARGET.shape)
 
 
 		coord.request_stop()
