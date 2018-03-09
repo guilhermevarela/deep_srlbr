0bbcd59054c86400edf080685413232ea0e5f8f0
Guilherme Varela <guilhermevarela@hotmail.com>
2018-03-08 21:29:11 -0300

    language model added
    Former-commit-id: 7ddca121412add664ad3ee76b14dcf577395a772

---
+++ models/lm.py
@@ -0,0 +1,29 @@
+'''
+	The language model 
+
+
+	1. for NER entities replace by the tag organization, person, location
+	2. for smaller than 5 tokens replace by one hot encoding 
+	3. include time i.e 20h30, 9h in number embeddings '0'
+	4. include ordinals 2º 2ª in number embeddings '0'
+	5. include tel._38-4048 in numeber embeddings '0'
+	New Word embedding size = embedding size + one-hot enconding of 2	
+
+
+'''
+class LanguageModel(object):
+	def __init__(self):
+	# load corpus
+	# apply
+	# load exception files on /corpus_exceptions
+
+	def get(feature):
+		'''
+
+		'''
+	def size(features):	
+		'''
+		
+		'''
+
+
