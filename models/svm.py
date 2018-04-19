631bc3f6e9773f02b3ef9c40b71b5dc98c3310e0
Guilherme Varela <guilhermevarela@hotmail.com>
2018-04-19 13:13:56 -0300

    Feature: engineering (2)

--- models/svm.py
+++ models/svm.py
@@ -91,7 +91,7 @@ if __name__ == '__main__':
 
     #(1, 3, 4, 5, 6, 7)
     #(0, 1, 2, 3, 4, 5, 6, 7)
-    for s in (1, 3, 4, 5, 6, 7):
+    for s in (0, 1, 2, 3, 4, 5, 6, 7):
         # optargs = '-s {:} -v 10'.format(s)
         optargs = '-s {:}'.format(s)
         print('Training ... with_optargs({:})'.format(optargs))
