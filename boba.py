1e4b1cfe74c494d22769f94ba9630ceee83ee7ae
Guilherme Varela <guilhermevarela@hotmail.com>
2018-06-27 20:08:03 -0300

    spn: perfect propositions additions

---
+++ boba.py
@@ -0,0 +1,18 @@
+from enum import Enum
+  
+class Color(Enum):
+    """This is simple color enumeration
+    
+    Maps colors to numbers
+    
+    Extends:
+        Enum
+    
+    Variables:
+        red {number} -- [description]
+        green {number} -- [description]
+        blue {number} -- [description]
+    """
+    red = 1
+    green = 2
+    blue = 3
