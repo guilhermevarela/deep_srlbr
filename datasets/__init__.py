57dd2689cbbb2723fd1ec311b54ad653c300e1df
Guilherme Varela <guilhermevarela@hotmail.com>
2018-08-01 19:59:16 -0300

    preprecessor: Feature factory

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,3 +1,3 @@
 from .scripts.tfrecords2 import input_fn, get_test, get_valid, get_train
 from .scripts.propbankbr import propbankbr_parser, propbankbr_parser2
-from .scripts.propbankbr import propbankbr_iob2arg, propbankbr_t2arg
+from .scripts.propbankbr import propbankbr_iob2arg, propbankbr_t2arg, propbankbr_arg2t, propbankbr_arg2iob
