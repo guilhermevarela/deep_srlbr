170d474f50169528012f4ea0124003626edfd305
Guilherme Varela <guilhermevarela@hotmail.com>
2018-08-07 19:04:38 -0300

    unittest: recover & persist

--- tests/test_propbankbr.py
+++ tests/test_propbankbr.py
@@ -20,11 +20,12 @@ import models.utils
 
 TESTS_DIR = os.path.dirname(os.path.realpath(__file__)) + '/'
 ROOT_DIR = os.getcwd()
-MOCK_DIR = '{:}mocks/'.format(TESTS_DIR)
+FIX_DIR = '{:}fixtures/'.format(TESTS_DIR)
+PICKLE_PATH = '{:}deep_glo50.pickle'.format(FIX_DIR)
 
 
 def _get_word2vec():
-    path_ = '{:}word2vec.json'.format(MOCK_DIR)
+    path_ = '{:}word2vec.json'.format(FIX_DIR)
     with open(path_, mode='r') as f:
         json_dict = json.load(f)
 
@@ -32,19 +33,17 @@ def _get_word2vec():
 
 
 def _get_gs():
-    path_ = '{:}gs.json'.format(MOCK_DIR)
+    path_ = '{:}gs.json'.format(FIX_DIR)
     with open(path_, mode='r') as f:
         gs_json = json.load(f)
 
-    return {keycol_: {
-                        int(key_): val_ 
-                        for key_, val_ in innerdict_.items()
-                     }
-                for keycol_, innerdict_ in gs_json.items()
-            }
+    return {
+        keycol_: {int(key_): val_ for key_, val_ in innerdict_.items()}
+        for keycol_, innerdict_ in gs_json.items()}
+
 
 def _get_schema():
-    schema_path = '{:}schema.yml'.format(MOCK_DIR)
+    schema_path = '{:}schema.yml'.format(FIX_DIR)
     with open(schema_path, mode='r') as f:
             schema_dict = yaml.load(f)
 
@@ -61,7 +60,22 @@ class PropbankTest(unittest.TestCase):
         models.utils.fetch_word2vec = unittest.mock.Mock(return_value=self.word2vec)
         self.propbank_encoder = \
             PropbankEncoder(self.gs_dict, self.schema_dict, language_model='glove_s50')
+        
 
-    def test_value(self):
-        print(self.propbank_encoder.db)
-        self.assertEqual(True, True)
+    def test_recover(self):
+        self.propbank_encoder.persist(FIX_DIR, 'deep_glo50')
+        propbank_encoder = PropbankEncoder.recover(PICKLE_PATH)
+        self.assertEqual(self.propbank_encoder, propbank_encoder)
+
+    def test_persist(self):
+
+        self.propbank_encoder.persist(FIX_DIR, 'mock_glo50')
+
+        fixture_path = '{:}mock_glo50.pickle'.format(FIX_DIR)
+        propbank_encoder = PropbankEncoder.recover(fixture_path)
+        self.assertEqual(self.propbank_encoder, propbank_encoder)
+        try:
+            import code; code.interact(local=dict(globals(), **locals()))
+            os.remove(fixture_path)
+        except Exception:
+            pass
