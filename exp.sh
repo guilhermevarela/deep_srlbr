3e56c849ffd6d04c43785bd37da89ef778ddc0f5
Varela <guilhermevarela@hotmail.com>
2018-09-26 09:56:54 -0300

    issue #13: Hyperparameter search R IOB

--- exp.sh
+++ exp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #This script runs a series of benchmark experiments
 python srl.py 16 16 --target R IOB --embs-model wrd50 --ru GRU --epochs 250
-#python srl.py 32 16 --target R IOB --embs-model wan300 --epochs 250 --batch_size 100
-#python srl.py 16 16 16 16 --target R IOB --embs-model wan100 --epochs 250 --batch_size 100
-#python srl.py 16 16 --target R IOB --embs-model glo50 --epochs 250 --batch_size 100
-#python srl.py 16 16 --target R IOB --embs-model wan50 --ru GRU --epochs 250 --batch_size 100
+python srl.py 32 16 --target R IOB --embs-model wan300 --epochs 250 --batch_size 100
+python srl.py 16 16 16 16 --target R IOB --embs-model wan100 --epochs 250 --batch_size 100
+python srl.py 16 16 --target R IOB --embs-model glo50 --epochs 250 --batch_size 100
+python srl.py 16 16 --target R IOB --embs-model wan50 --ru GRU --epochs 250 --batch_size 100
