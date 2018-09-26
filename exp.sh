7893488f3ca157734bd2c6b82563885c428d8ac7
Guilherme Varela <guilhermevarela@hotmail.com>
2018-09-26 08:16:24 -0300

    issue #13: Hyperparameter search R T

--- exp.sh
+++ exp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #This script runs a series of benchmark experiments
-python srl.py 16 16 --target R IOB --embs-model wrd50 --ru GRU --epochs 500
-python srl.py 16 16 --target R T --embs-model wan300 --epochs 500
-python srl.py 16 16 16 16 --target R IOB --embs-model wan100 --epochs 500
-python srl.py 16 16 --target R T --embs-model glo50 --epochs 500
-python srl.py 16 16 --target R T --embs-model wan50 --ru GRU --epochs 500
+python srl.py 16 16 --target R T --embs-model wrd50 --ru GRU --epochs 250
+python srl.py 32 16 --target R T --embs-model wan300 --epochs 250 --batch_size 100
+python srl.py 16 16 16 16 --target R T --embs-model wan100 --epochs 250 --batch_size 100
+python srl.py 16 16 --target R T --embs-model glo50 --epochs 250 --batch_size 100
+python srl.py 16 16 --target R T --embs-model wan50 --ru GRU --epochs 250 --batch_size 100
